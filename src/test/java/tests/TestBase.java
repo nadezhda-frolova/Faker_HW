8eee4ab9b6366d518f4387e41aafe6f961d27ff4
Nadya Frolova <n.frolova.kzn@yandex.ru>
2023-02-17 11:24:10 +0300

    faker перенесен из TestBase в FillUserDataTest.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,21 +3,21 @@ package tests;
 import com.github.javafaker.Faker;
 import pages.RegistrationPage;
 public class TestBase {
-    Faker faker = new Faker();
+
     RegistrationPage registrationPage = new RegistrationPage();
-    String userName = faker.name().firstName();
-    String userSurname = faker.name().lastName();
-    String userEmail = faker.internet().emailAddress();
-    String userGender = "Female";
-    String userPhone = faker.phoneNumber().subscriberNumber(10);
-    String userBirthday = "04";
-    String userBirthMonth = "November";
-    String userBirthYear = "1984";
-    String subject = "Maths";
-    String hobbies = "Sports";
-    String picture = "frog.jpg";
-    String currentAddress = faker.address().fullAddress();
-    String state = "NCR";
-    String city = "Delhi";
+    String userName;
+    String userSurname;
+    String userEmail;
+    String userGender;
+    String userPhone;
+    String userBirthday;
+    String userBirthMonth;
+    String userBirthYear;
+    String subject;
+    String hobbies;
+    String picture;
+    String currentAddress;
+    String state;
+    String city;
 
 }
